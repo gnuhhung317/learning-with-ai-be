299df6174c27512958fbb22cf4dc788d1beeca3f
Duc Hung <duchung04st@gmail.com>
2025-02-19 11:08:15 +0700

    listening from internet

--- src/index.js
+++ src/index.js
@@ -72,6 +72,6 @@ app.post('/api/quiz/pdf/generate', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0',() => {
   console.log(`Server is running on port ${port}`);
 });
